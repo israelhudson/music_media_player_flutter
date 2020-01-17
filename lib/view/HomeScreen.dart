cfa3924981d2d2c0ffc02c715620b83dc69244fa
israelhudson <israelhudson@gmail.com>
2020-01-16 22:53:38 -0300

    testando listas

--- lib/view/HomeScreen.dart
+++ lib/view/HomeScreen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:music_media_player_flutter/helpers/filesystem_utils.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,14 +10,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String tempPath;
+  List<Directory> list = List();
+
+  getListDirectories() async {
+//    Directory tempDir = await getListDirectories();
+//    setState(() {
+//      tempPath = tempDir.path;
+//    });
+    getStorageList().then((data) {
+      setState(() {
+
+        data.forEach((f) {
+          //tempPath += "_______${f}______\n";
+          tempPath = f.toString();
+
+        });
+
+        list = data;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    getListDirectories();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Player de Música", style: TextStyle(color: Colors.white),),
+        title: Text(
+          "Player de Música ${list.length}",
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: getListDirectories,
+        ),
         backgroundColor: Colors.teal,
       ),
       body: Container(
+        //child: Text("${tempPath}"),
+        child: ListView.builder(
+            itemBuilder: (context, _) {
+              return Container(
+                child: Text("${tempPath}"),
+              );
+            },
+            itemCount: list.length),
       ),
     );
   }
